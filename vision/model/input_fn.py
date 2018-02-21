5c3bae2adab9a7a68f8b8acfe4887324423da84b
Neel Y <Neel@DN0a0fde95.SUNet>
2018-02-20 23:50:10 -0800

    edited pipelines

--- vision/model/input_fn.py
+++ vision/model/input_fn.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 
 
-def _parse_function(filename, label, size):
+def _parse_function(filename, size=180):
     """Obtain the image from the filename (for both training and validation).
 
     The following operations are applied:
@@ -13,14 +13,14 @@ def _parse_function(filename, label, size):
     image_string = tf.read_file(filename)
 
     # Don't use tf.image.decode_image, or the output shape will be undefined
-    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
+    image_decoded = tf.image.decode_png(image_string, channels=3)
 
     # This will convert to float values in [0, 1]
     image = tf.image.convert_image_dtype(image_decoded, tf.float32)
 
     resized_image = tf.image.resize_images(image, [size, size])
 
-    return resized_image, label
+    return resized_image
 
 
 def train_preprocess(image, label, use_random_flip):
@@ -56,23 +56,20 @@ def input_fn(is_training, filenames, labels, params):
         params: (Params) contains hyperparameters of the model (ex: `params.num_epochs`)
     """
     num_samples = len(filenames)
-    assert len(filenames) == len(labels), "Filenames and labels should have same length"
 
     # Create a Dataset serving batches of images and labels
     # We don't repeat for multiple epochs because we always train and evaluate for one epoch
-    parse_fn = lambda f, l: _parse_function(f, l, params.image_size)
-    train_fn = lambda f, l: train_preprocess(f, l, params.use_random_flip)
+    parse_fn = lambda f: _parse_function(f, params.image_size)
 
     if is_training:
-        dataset = (tf.data.Dataset.from_tensor_slices((tf.constant(filenames), tf.constant(labels)))
+        dataset = (tf.data.Dataset.from_tensor_slices((tf.constant(filenames)))
             .shuffle(num_samples)  # whole dataset into the buffer ensures good shuffling
             .map(parse_fn, num_parallel_calls=params.num_parallel_calls)
-            .map(train_fn, num_parallel_calls=params.num_parallel_calls)
             .batch(params.batch_size)
             .prefetch(1)  # make sure you always have one batch ready to serve
         )
     else:
-        dataset = (tf.data.Dataset.from_tensor_slices((tf.constant(filenames), tf.constant(labels)))
+        dataset = (tf.data.Dataset.from_tensor_slices((tf.constant(filenames)))
             .map(parse_fn)
             .batch(params.batch_size)
             .prefetch(1)  # make sure you always have one batch ready to serve
@@ -83,5 +80,5 @@ def input_fn(is_training, filenames, labels, params):
     images, labels = iterator.get_next()
     iterator_init_op = iterator.initializer
 
-    inputs = {'images': images, 'labels': labels, 'iterator_init_op': iterator_init_op}
+    inputs = {'images': images, 'iterator_init_op': iterator_init_op}
     return inputs
--- vision/train.py
+++ vision/train.py
@@ -16,9 +16,9 @@ from model.training import train_and_evaluate
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_dir', default='experiments/test',
+parser.add_argument('--model_dir', default='experiments/base_model',
                     help="Experiment directory containing params.json")
-parser.add_argument('--data_dir', default='data/64x64_SIGNS',
+parser.add_argument('--data_dir', default='data/train_first_images',
                     help="Directory containing the dataset")
 parser.add_argument('--restore_from', default=None,
                     help="Optional, directory or file containing weights to reload before training")
@@ -36,9 +36,9 @@ if __name__ == '__main__':
 
     # Check that we are not overwriting some previous experiment
     # Comment these lines if you are developing your model and don't care about overwritting
-    model_dir_has_best_weights = os.path.isdir(os.path.join(args.model_dir, "best_weights"))
-    overwritting = model_dir_has_best_weights and args.restore_from is None
-    assert not overwritting, "Weights found in model_dir, aborting to avoid overwrite"
+    # model_dir_has_best_weights = os.path.isdir(os.path.join(args.model_dir, "best_weights"))
+    # overwritting = model_dir_has_best_weights and args.restore_from is None
+    # assert not overwritting, "Weights found in model_dir, aborting to avoid overwrite"
 
     # Set the logger
     set_logger(os.path.join(args.model_dir, 'train.log'))
@@ -46,31 +46,29 @@ if __name__ == '__main__':
     # Create the input data pipeline
     logging.info("Creating the datasets...")
     data_dir = args.data_dir
-    train_data_dir = os.path.join(data_dir, "train_signs")
-    dev_data_dir = os.path.join(data_dir, "dev_signs")
+    train_data_dir = data_dir
+    # dev_data_dir = os.path.join(data_dir, "dev_signs")
 
     # Get the filenames from the train and dev sets
     train_filenames = [os.path.join(train_data_dir, f) for f in os.listdir(train_data_dir)
-                       if f.endswith('.jpg')]
-    eval_filenames = [os.path.join(dev_data_dir, f) for f in os.listdir(dev_data_dir)
-                      if f.endswith('.jpg')]
+                       if f.endswith('.png')]
+    # eval_filenames = [os.path.join(dev_data_dir, f) for f in os.listdir(dev_data_dir)
+    #                   if f.endswith('.png')]
 
     # Labels will be between 0 and 5 included (6 classes in total)
-    train_labels = [int(f.split('/')[-1][0]) for f in train_filenames]
-    eval_labels = [int(f.split('/')[-1][0]) for f in eval_filenames]
 
     # Specify the sizes of the dataset we train on and evaluate on
     params.train_size = len(train_filenames)
-    params.eval_size = len(eval_filenames)
+    # params.eval_size = len(eval_filenames)
 
     # Create the two iterators over the two datasets
     train_inputs = input_fn(True, train_filenames, train_labels, params)
-    eval_inputs = input_fn(False, eval_filenames, eval_labels, params)
+    # eval_inputs = input_fn(False, eval_filenames, eval_labels, params)
 
     # Define the model
     logging.info("Creating the model...")
     train_model_spec = model_fn('train', train_inputs, params)
-    eval_model_spec = model_fn('eval', eval_inputs, params, reuse=True)
+    # eval_model_spec = model_fn('eval', eval_inputs, params, reuse=True)
 
     # Train the model
     logging.info("Starting training for {} epoch(s)".format(params.num_epochs))
